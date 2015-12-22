b9648a51e958f696a33664a8fa8545d47da02d54
Björn Guth <bjoern.guth@rwth-aachen.de>
2015-12-22 14:43:12 +0100

    created small program to test pupil_size() from libtobii

---
+++ test.py
@@ -0,0 +1,33 @@
+from pygaze import eyetracker, libscreen, libtime, libinput
+from constants import *
+
+display = libscreen.Display()
+screen = libscreen.Screen()
+
+keyboard = libinput.Keyboard()
+
+tracker = eyetracker.EyeTracker(display)
+screen.draw_text('calibrate? [y,n]','red')
+display.fill(screen)
+display.show()
+key,time = keyboard.get_key(['y','n'],None)
+print(key)
+if key == 'y':
+	tracker.calibrate()
+else:
+	tracker.pxfixtresh = 60.
+
+tracker.start_recording()
+
+while True:
+	info_text = 'pos: {:>15} | pupil size: {:<20}'.format(tracker.sample(),tracker.pupil_size())
+	screen.clear()
+	screen.draw_text(info_text,'black')
+	display.fill(screen)
+	display.show()
+	print(info_text)
+	libtime.pause(1000)
+
+
+
+print('test')
