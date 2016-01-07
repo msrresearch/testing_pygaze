3c0a49dc177d38e14309c73e3a0f9034e2be8b23
Björn Guth <bjoern.guth@rwth-aachen.de>
2016-01-07 17:24:13 +0100

    added some stuff to the output

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
 from pygaze import eyetracker, libscreen, libtime, libinput
 from constants import *
 
+import threading
+
 display = libscreen.Display()
 screen = libscreen.Screen()
 
 keyboard = libinput.Keyboard()
 
 tracker = eyetracker.EyeTracker(display)
+
+print(tracker.spdtresh)
+print(tracker.accthresh)
+print(tracker.eventdetection)
+
 screen.draw_text('calibrate? [y,n]','red')
 display.fill(screen)
 display.show()
@@ -19,11 +26,37 @@ else:
 
 tracker.start_recording()
 
+class Blink_Detector(threading.Thread):
+	def __init__(self, tracker, name='Blink_Detector'):
+		super(Blink_Detector,self).__init__(name=name)
+		self.tracker = tracker
+		self.daemon = True
+		self.blink_start = 0.
+		self.blink_end = 0.
+		self.blink_dur = 0.
+	
+	def run(self):
+		while True:
+			self.blink_start = self.tracker.wait_for_blink_start()
+			self.blink_end = self.tracker.wait_for_blink_end()
+			self.blink_dur = self.blink_end - self.blink_start
+
+bd = Blink_Detector(tracker)
+bd.start()
+
+
 while True:
-	info_text = 'pos: {:>15} | 3D gaze point: {:<20}'.format(tracker.sample(),tracker.gaze_vector())
+	pos = tracker.sample()
+	gaze_vector = tracker.gaze_vector()
+	info_text = 'pos: {:>15}'.format(pos)
+	info_text += '\neye position: {:<60}'.format(tracker.eye_position_3d())
+	info_text += '\n3D gaze vector: {:<90}'.format(gaze_vector)
+	info_text += '\npupil size: {:>15}'.format(tracker.pupil_size())
+	info_text += '\nblink_start: {:>15} | blink_end: {:>15} | blink_dur: {:>15}'.format(bd.blink_start,bd.blink_end,bd.blink_dur)
 	screen.clear()
 	screen.draw_text(info_text,'black')
+	screen.draw_fixation(colour='red',pos=pos)
 	display.fill(screen)
 	display.show()
 	print(info_text)
-	libtime.pause(1000)
+	libtime.pause(10)
